47ca0da127770e21402c4b88f2dabcfe87c602fa
Mike Goodspeed <mgoodspeed@getcollc.com>
2011-05-21 10:02:07 +0100

    Initial Commit

---
+++ GraphCalc/GraphCalcAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GraphCalcAppDelegate.h
+//  GraphCalc
+//
+//  Created by Mike Goodspeed on 5/21/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphCalcAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
